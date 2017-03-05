4c2aab07fe8adef0b1f3c7e6cebf0b0a3e3ba18c
DavidMichaelPena <alexamenos@gmail.com>
2017-03-05 01:11:32 +0000

    adding Factory Girl

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -14,13 +14,9 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to redirect_to new_user_session_path
     end
 
-    it "shouls successfully show the new form" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-        )
-        sign_in user
+    it "should successfully show the new form" do
+      user = FactoryGirl.create(:user)
+      sign_in user
 
       get :new
       expect(response).to have_http_status(:success)
@@ -36,11 +32,7 @@ RSpec.describe GramsController, type: :controller do
 
 
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       post :create, params: { gram: { message: 'Hello!' } }
@@ -53,11 +45,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       gram_count = Gram.count
